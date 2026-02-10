51bf900aa1d702ca2364c96a597738e2ca99ead7
naishaasapre-rgb <naishaasapre@gmail.com>
2026-02-11 00:14:02 +0530

    Delete all content from q1.c
    Remove the entire content of q1.c file.

--- Naishaa/q1.c
+++ Naishaa/q1.c
@@ -1,15 +1,1 @@
-#include<stdio.h>
- int main()
-{
-    int i,j;
-    for(i=1;i<=5;i++){
-        for(j=4;j>=i;j--){
-            printf("\t");
-        }
-        for(int k=1;k<=i;k++){
-            printf("*\t");
-        }
-        printf("\n");
-    }
-    return 0;
-}
+
