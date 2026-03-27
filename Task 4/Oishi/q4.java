2122223e83963d4ca57376f9b92b2d36deeb5e2e
Oishi2209 <oishide2209@gmail.com>
2026-03-27 19:23:22 +0530

    Binary tree pre-order traversal

---
+++ Task 4/Oishi/q4.java
@@ -0,0 +1,33 @@
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+public class q4{
+
+    public static void preOrder(Node node) {
+        if (node == null)
+            return;
+
+        System.out.println(node.data); 
+        preOrder(node.left);   
+        preOrder(node.right);  
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.right = new Node(6);
+
+        preOrder(root);
+    }
+}
