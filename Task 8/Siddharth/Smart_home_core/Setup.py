a29177d776f1853d9f8a863ca185f5334459cd16
stillwave-code <siddharthprasad61@gmail.com>
2026-06-27 00:10:31 +0530

    Update Setup.py

--- Task 8/Siddharth/Smart_home_core/Setup.py
+++ Task 8/Siddharth/Smart_home_core/Setup.py
@@ -1,1 +1,34 @@
 
+from setuptools import find_packages, setup
+
+package_name = 'smart_home_core'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    package_data={'': ['py.typed']},
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='siddharth-prasad',
+    maintainer_email='siddharth-prasad@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+	    'temp_sensor = smart_home_core.temp_sensor_node:main',
+            'smart_plug = smart_home_core.smart_plug_node:main',
+            'dashboard = smart_home_core.dashboard_node:main',
+        ],
+    },
+)
